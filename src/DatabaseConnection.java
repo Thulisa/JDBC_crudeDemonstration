40a68a6b7a19758b82789e9d463217334e7f7f40
New File <thulzbakaqana@gmail.com>
2016-08-01 10:42:53 +0200

    initial commit

---
+++ src/DatabaseConnection.java
@@ -0,0 +1,13 @@
+/**
+ * Created by bakthu01 on 7/28/2016.
+ */
+
+import  java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+
+
+}
